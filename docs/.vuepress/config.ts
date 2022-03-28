9157cdb7020f394691bd49e68aeeb0e128ee2e12
pakholam <1299561671@qq.com>
2022-03-28 18:19:41 +0800

    修复版本更新日志按钮无法正确跳转

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -6,8 +6,7 @@ import { navbar, sidebar } from "./configs";
 const isProd = process.env.NODE_ENV === "production";
 
 export default defineUserConfig<DefaultThemeOptions>({
-  base: "/",
-
+  base: "/vuepress-note/",
   head: [
     [
       "link",
@@ -52,11 +51,6 @@ export default defineUserConfig<DefaultThemeOptions>({
       title: "Cerf-Blog",
       description: "Vuepress Blog",
     },
-    "/zh/": {
-      lang: "zh-CN",
-      title: "鹿博",
-      description: "Vuepress 博客",
-    },
   },
 
   bundler: process.env.DOCS_BUNDLER ?? "@vuepress/vite",
@@ -76,34 +70,6 @@ export default defineUserConfig<DefaultThemeOptions>({
 
         editLinkText: "Edit this page on Github",
       },
-      // "/zh/": {
-      //   navbar: navbar.zh,
-      //   selectLanguageName: "简体中文",
-      //   selectLanguageText: "选择语言",
-      //   selectLanguageAriaLabel: "选择语言",
-
-      //   sidebar: sidebar.zh,
-
-      //   editLinkText: "在GitHub上编辑此页",
-      //   lastUpdatedText: "上次更新",
-      //   contributorsText: "贡献者",
-
-      //   tip: "提示",
-      //   warning: "注意",
-      //   danger: "警告",
-
-      //   notFound: [
-      //     "这里什么都没有",
-      //     "我们怎么到这来了？",
-      //     "这是一个404页面",
-      //     "看起来我们进入了错误的链接",
-      //   ],
-      //   backToHome: "返回首页",
-
-      //   openInNewWindow: "在新窗口打开",
-      //   toggleDarkMode: "切换夜间模式",
-      //   toggleSidebar: "切换侧边栏",
-      // },
     },
 
     themePlugins: {
@@ -123,65 +89,6 @@ export default defineUserConfig<DefaultThemeOptions>({
   },
 
   plugins: [
-    // [
-    //   "@vuepress/plugin-docsearch",
-    //   {
-    //     apiKey: "3a539aab83105f01761a137c61004d85",
-    //     indexName: "Cerf-Blog",
-    //     searchParameters: {
-    //       facetFilters: ["tags:v2"],
-    //     },
-    //     locales: {
-    //       "/zh/": {
-    //         placeholder: "搜索文档",
-    //         translations: {
-    //           button: {
-    //             buttonText: "搜索文档",
-    //             buttonAriaLabel: "搜索文档",
-    //           },
-    //           modal: {
-    //             searchBox: {
-    //               resetButtonTitle: "清除查询条件",
-    //               resetButtonAriaLabel: "清除查询条件",
-    //               cancelButtonText: "取消",
-    //               cancelButtonAriaLabel: "取消",
-    //             },
-    //             startScreen: {
-    //               recentSearchesTitle: "搜索历史",
-    //               noRecentSearchesText: "没有搜索历史",
-    //               saveRecentSearchButtonTitle: "保存至搜索历史",
-    //               removeRecentSearchButtonTitle: "从搜索历史中移除",
-    //               favoriteSearchesTitle: "收藏",
-    //               removeFavoriteSearchButtonTitle: "从收藏中移除",
-    //             },
-    //             errorScreen: {
-    //               titleText: "无法获取结果",
-    //               helpText: "你可能需要检查你的网络连接",
-    //             },
-    //             footer: {
-    //               selectText: "选择",
-    //               navigateText: "切换",
-    //               closeText: "关闭",
-    //               searchByText: "搜索提供者",
-    //             },
-    //             noResultsScreen: {
-    //               noResultsText: "无法找到相关结果",
-    //               suggestedQueryText: "你可以尝试查询",
-    //               openIssueText: "你认为该查询应该有结果？",
-    //               openIssueLinkText: "点击反馈",
-    //             },
-    //           },
-    //         },
-    //       },
-    //     },
-    //   },
-    // ],
-    // [
-    //   '@vuepress/plugin-google-analytics',
-    //   {
-    //     id: process.env.DOCS_GA_ID,
-    //   },
-    // ],
     [
       '@vuepress/plugin-search',
       {
